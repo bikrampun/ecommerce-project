32b1f1c74cc4dd9275f8f342bd0a92ee7214808f
Bikram Pun <bikrampun47@gmail.com>
2023-01-27 13:50:23 +0545

    Added CrossOrigin support to SpringBoot due to restriction of same origin policy on web browser

--- 02-backend/spring-boot-ecommerce/src/main/java/com/learning/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/learning/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,11 @@ package com.learning.ecommerce.dao;
 import com.learning.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//web browser will not allow script code to call APIs not on the same origin due to same origin policy
+// @CrossOrigin("http://localhost:4200") - allow cross origin requests from http://localhost:4200
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 // productCategory -- name of json entry and /product-category will be actual reference for the path
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
