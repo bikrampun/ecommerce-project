7df568766e7d26505439c11dfa61405815b50ca5
Bikram Pun <bikrampun47@gmail.com>
2023-01-31 23:11:23 +0545

    Query method added to REST Repository

--- 02-backend/spring-boot-ecommerce/src/main/java/com/learning/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/learning/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.learning.ecommerce.dao;
 
 import com.learning.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    // Spring will execute query similar to this
+    // select * from product where category_id=?
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
