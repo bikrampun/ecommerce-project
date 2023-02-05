ca981ad861ebdc37384a2a4def4139f7404ac344
Bikram Pun <bikrampun47@gmail.com>
2023-02-05 20:56:39 +0545

    Add new search method

--- 02-backend/spring-boot-ecommerce/src/main/java/com/learning/ecommerce/dao/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/learning/ecommerce/dao/ProductRepository.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // Spring will execute query similar to this
-    // select * from product where category_id=?
+    // select * from Product where category_id=?
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    // Spring will execute query similar to this
+    // select * from Product p where p.name like concat('%', :name, '%')
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
