ffde4faea8f0cf0d7b147d47b0875c3e18b799e1
Bikram Pun <bikrampun47@gmail.com>
2023-02-07 16:21:46 +0545

    getProduct method added

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -20,6 +20,14 @@ export class ProductService {
   
   // inject HttpClient into our service
   constructor(private httpClient: HttpClient) { }
+
+  getProduct(theProductId: number): Observable<Product> {
+    
+    //need to build URL based on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    
+    return this.httpClient.get<Product>(productUrl);
+  }
   
   getProductList(theCategoryId: number): Observable<Product[]> {
     
