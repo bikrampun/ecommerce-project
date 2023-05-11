41d617696a6dbfaf395b9e911a72aee26477e8db
Bikram Pun <bikrampun47@gmail.com>
2023-05-11 14:17:59 +0545

    Modify to call CartService

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -25,6 +27,7 @@ export class ProductListComponent {
   previouskeyword: string | undefined;
 
   constructor(private productService: ProductService,
+                private cartService: CartService,
                 private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -132,6 +135,10 @@ export class ProductListComponent {
     console.log(`Add to Cart: ${theProduct.name}, ${theProduct.unitPrice}`);
 
     // TODO ... do the real work
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
+
   }
 
 }
