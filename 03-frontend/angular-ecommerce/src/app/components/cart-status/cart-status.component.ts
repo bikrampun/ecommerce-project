a9a3a73b9fac577ca8742f2c9b45686528c0b3f4
Bikram Pun <bikrampun47@gmail.com>
2023-05-12 14:25:51 +0545

    Enhance to subscribe to CartService and Updated to display cart total price and quantity

--- 03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/cart-status/cart-status.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-status',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class CartStatusComponent {
 
+  totalPrice: number = 0.0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    
+    // subscribe to the cart totalPrize
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+
+    // subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    );
+
+  }
 }
